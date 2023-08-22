586c87ce52197a5958628e37b0b90dec765652ae
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2023-08-22 17:10:01 +0200

    adding x for sharing instead of twitter

--- templates/sharing.php
+++ templates/sharing.php
@@ -6,8 +6,8 @@
 		<?php echo elb_get_social_logo( 'facebook' ); ?>
 	</a>
 
-	<a href="https://twitter.com/intent/tweet?text=<?php elb_entry_title(); ?> <?php echo elb_get_entry_url(); ?>" target="_blank" title="<?php _e( 'Share via Twitter', ELB_TEXT_DOMAIN ); ?>">
-		<?php echo elb_get_social_logo( 'twitter-alt' ); ?>
+	<a href="https://x.com/intent/tweet?text=<?php elb_entry_title(); ?> <?php echo elb_get_entry_url(); ?>" target="_blank" title="<?php _e( 'Share via X/Twitter', ELB_TEXT_DOMAIN ); ?>">
+		<?php echo elb_get_social_logo( 'x' ); ?>
 	</a>
 
 	<a href="mailto:?&subject=<?php elb_entry_title(); ?>&body=<?php echo elb_get_entry_url(); ?>" target="_blank" title="<?php _e( 'Share via email', ELB_TEXT_DOMAIN ); ?>">
